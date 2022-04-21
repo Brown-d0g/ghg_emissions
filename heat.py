fa2bd0b85a9ec317b10190372c80e864d1ca8fb0
cd <cd@desk.com>
2022-04-21 11:32:44 -0700

    have map!

--- heat.py
+++ heat.py
@@ -3,76 +3,97 @@
 # bst, but good
 # classes are cancelled
 
-# a bst is a list of a data element and a list of a less bst, and a more bst
-# or its a []
-# then we store the key in last spot as a performance enhancer
-# so [key, value, kids]
+# a bst is a list - less, more, key, vals
+# key is a name
+# vals is a year indexed list of sets
 
-def insert(data, tree, func):
-	key = func(data)
-	if not key:
+def insert(data, tree, func, year):
+	key, val = func(data)
+	if not val:
 		return
-	while tree:
-		tree = tree[2][key > tree[0]]
-	tree.extend([key, data, [[], []]])
-	
-def contains(key, tree):
-	if not key:
-		return
-	while tree:
-		if not tree:
-			return
-		if key == tree[0]:
-			return tree[0:2]
-		tree = tree[2][key > tree[0]]
+	# traverse
+	while tree and key != tree[2]:
+		tree = tree[key > tree[2]]
+	if tree:
+	# add value to key
+		tree[3][year].add(val)
+	elif not year:
+	# add key to tree
+		tree.extend([[],[], key, [{val},set()]])
 		
-# go in as long as you can then grab an element
-def pop(tree):
-	while any(tree[2]):
-		if tree[2][0]:
-			tree = tree[2][0]
-		else:
-			tree = tree[2][1]
-	ret, _ = tree[0:2], tree.clear()
-	return ret
-
-# csv to Python, using bst by default
-def csv_to_p(file, func):
-	tree = []
-	[insert(line, tree, func) for line in file]
-	return tree
+def tolist(tree):
+	if tree:
+		# we have a two sets and want to add all pairs between sets
+		return tolist(tree[0]) + [(one, two) for one in tree[3][0] for two in tree[3][1]] + tolist(tree[1])
+	return []
 
 # file handling
 
 # line to key
-def get_key(l):
+def get_kv(l):
 	# we need the 13th column but quotes mix it up
 	key = ""
+	val = ""
 	commas = 0
 	quotes = False
 	for c in l:
 		if c == '\"':
 			quotes = not quotes
-		if c == ',' and not quotes:
+		elif c == ',' and not quotes:
 			commas += 1
-		if commas == 13 and c.isalpha():
+		elif commas == 13:
 			key += c
-		if commas == 14:
-			# wipe out punctuation
-			return key
+		elif commas == 1 and (c.isupper() or c.isspace()):
+			val += c
+		elif commas == 14:
+			# shorten val
+			if "FOR" in val:
+				val = val[:val.find("FOR")]
+			if "PRES" in val:
+				val = val[:val.find("PRES")]
+			if "AMER" in val:
+				val = val[:val.find("AMER")]
+			val = val.replace("FRIENDS OF","")
+			val = val.replace("COMMITTEE","")
+			val = val.replace("TO ELECT","")
+			val = val.replace("INC","")
+			val = val.replace("CAMPAIGN","")
+			val.strip()
+			return key, val
 
-def main():
+# make the dictionary
+def getd():
 	# compare two cycles
 	csvs = ["2016_2700s.csv","2020_2800s.csv"]
-	wood = [csv_to_p(open(s,"r"),get_key) for s in csvs]
 	# with two bsts, we check for shared entries
-	twos = []
-	while wood[0]:
-		curr = pop(wood[0])
-		both = contains(curr[0], wood[1])
-		if both:
-			twos.append(curr + [both[1]])
-	print(len(twos))
-	print(twos[0])
+	tree = []
+	[insert(line, tree, get_kv, n) for n in [0,1] for line in open(csvs[n],"r")]
+	twos = [donr for donr in tolist(tree) if all(donr)]
+	d = { one : { two : 0 for two in set([donr[1] for donr in twos]) } for one in set([donr[0] for donr in twos]) }
+	for donr in twos:
+		d[donr[0]][donr[1]] += 1
+	for one in d:
+		for two in d[one]:
+			d[one][two] = d[one][two].bit_length().bit_length()
+	return d
+
+# we need a few imports to make the map
+
+import pandas
+import seaborn
+import matplotlib.pyplot as plt
+
+def heat():
+
+	df = pandas.DataFrame.from_dict(getd())
 	
-main()
+	blues = seaborn.color_palette("light:b", as_cmap=True)
+	plt.rcParams['figure.figsize'] = [10, 10]
+	plt.rcParams['figure.dpi'] = 100
+	plt.tick_params(axis='both', which='major', labelsize=10, labelbottom = False, bottom=False, top = False, left = False, labeltop=True)
+	
+	p = seaborn.heatmap(df)
+	f = p.get_figure()
+	f.savefig("heat.png",bbox_inches='tight')
+	
+heat()
