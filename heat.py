2f11137650251acfdf62c47ce49630ba922b770c
cd <cd@desk.com>
2022-04-21 12:53:46 -0700

    update writeup

--- heat.py
+++ heat.py
@@ -3,10 +3,6 @@
 # bst, but good
 # classes are cancelled
 
-# a bst is a list - less, more, key, vals
-# key is a name
-# vals is a year indexed list of sets
-
 def insert(data, tree, func, year):
 	key, val = func(data)
 	if not val:
@@ -24,7 +20,8 @@ def insert(data, tree, func, year):
 def tolist(tree):
 	if tree:
 		# we have a two sets and want to add all pairs between sets
-		return tolist(tree[0]) + [(one, two) for one in tree[3][0] for two in tree[3][1]] + tolist(tree[1])
+		l = [(one, two) for one in tree[3][0] for two in tree[3][1]]
+		return tolist(tree[0]) + l + tolist(tree[1])
 	return []
 
 # file handling
@@ -32,10 +29,7 @@ def tolist(tree):
 # line to key
 def get_kv(l):
 	# we need the 13th column but quotes mix it up
-	key = ""
-	val = ""
-	commas = 0
-	quotes = False
+	key, val, commas, quotes = "", "", 0, False
 	for c in l:
 		if c == '\"':
 			quotes = not quotes
@@ -47,34 +41,33 @@ def get_kv(l):
 			val += c
 		elif commas == 14:
 			# shorten val
-			if "FOR" in val:
-				val = val[:val.find("FOR")]
-			if "PRES" in val:
-				val = val[:val.find("PRES")]
-			if "AMER" in val:
-				val = val[:val.find("AMER")]
-			val = val.replace("FRIENDS OF","")
-			val = val.replace("COMMITTEE","")
-			val = val.replace("TO ELECT","")
-			val = val.replace("INC","")
-			val = val.replace("CAMPAIGN","")
+			for f in ["FOR", "PRES", "AMER"]:
+				if f in val:
+					val = val[:val.find(f)]
+			for r in ["FRIENDS OF", "COMMITTEE", "TO ELECT", "INC", "CAMPAIGN"]:
+				val = val.replace(r,"")
 			val.strip()
 			return key, val
 
 # make the dictionary
 def getd():
-	# compare two cycles
+	# csv -> bst
 	csvs = ["2016_2700s.csv","2020_2800s.csv"]
-	# with two bsts, we check for shared entries
 	tree = []
 	[insert(line, tree, get_kv, n) for n in [0,1] for line in open(csvs[n],"r")]
-	twos = [donr for donr in tolist(tree) if all(donr)]
-	d = { one : { two : 0 for two in set([donr[1] for donr in twos]) } for one in set([donr[0] for donr in twos]) }
-	for donr in twos:
-		d[donr[0]][donr[1]] += 1
-	for one in d:
-		for two in d[one]:
-			d[one][two] = d[one][two].bit_length().bit_length()
+	
+	# bst -> list
+	dnrs = [donr for donr in tolist(tree) if all(donr)] # ensure donations both cycles
+	
+	# list -> dict
+	sxtn, twnt = set([donr[0] for donr in dnrs]), set([donr[1] for donr in dnrs])
+	d = { s : { t : 0 for t in twnt } for s in sxtn } # init to zero
+	for donr in dnrs:
+		d[donr[0]][donr[1]] += 1 # compute frequencies 
+		
+	# frequency -> log of log of frequency
+	d = { s : { t : d[s][t].bit_length().bit_length() for t in twnt } for s in sxtn }
+			
 	return d
 
 # we need a few imports to make the map
@@ -87,13 +80,16 @@ def heat():
 
 	df = pandas.DataFrame.from_dict(getd())
 	
-	blues = seaborn.color_palette("light:b", as_cmap=True)
+	mako = seaborn.color_palette("mako", as_cmap=True)
 	plt.rcParams['figure.figsize'] = [10, 10]
 	plt.rcParams['figure.dpi'] = 100
 	plt.tick_params(axis='both', which='major', labelsize=10, labelbottom = False, bottom=False, top = False, left = False, labeltop=True)
+	plt.style.use("dark_background")
 	
-	p = seaborn.heatmap(df)
+	p = seaborn.heatmap(df, cmap=mako)
+	p.xaxis.label.set_color('purple')
+	p.yaxis.label.set_color('silver')
 	f = p.get_figure()
-	f.savefig("heat.png",bbox_inches='tight')
+	f.savefig("heat.png",bbox_inches='tight',transparent=True)
 	
 heat()
